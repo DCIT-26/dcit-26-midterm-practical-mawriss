64717a026aa716e2487877c153214492c59df5b3
Maurice Mojica <mauricemojica20@gmail.com>
2025-10-29 21:14:50 +0800

    DCIT 26 Application Development

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-print("DCIT 26 – Midterm Practical Laboratory")
-
+print("DCIT 26 - Midterm Practical Laboratory")
 print("Submitted by Mojica, Maurice")
-
-print("Date of Submission: October 28, 2025")
-
